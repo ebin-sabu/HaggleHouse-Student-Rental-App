f0fc4077f398eeca8eba47cd5c7300b6b8138b04
ebin-sabu <ebin.pereppadan01@gmail.com>
2024-01-28 22:07:34 +0000

    more css changes for mobile

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,41 @@
 import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-const Navbar = () => {
+const NavbarCustom = () => {
 
   return (
-    <header>
-      <div className="MainNavbar">
-        <Link to="/">
-          <h1>< span class="highlight">H</span>aggle< span class="highlight">H</span>ouse</h1>
-        </Link>
-        <Link to="/login"><h2>Login</h2></Link>
-      </div>
-    </header>
+
+    <Navbar expand="lg" sticky="top" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+      <Container fluid>
+        <Navbar.Brand href="/">
+          <img
+            alt="Logo"
+            src="/logo.png"
+            width="70"
+            height="70"
+            className="d-inline-block align-top"
+          />{' '}
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/"><h1 className='title'>< span className="highlight">H</span>aggle<span className="highlight">H</span>ouse</h1> </Nav.Link>
+        </Nav>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="ms-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav className="ms-auto">
+              <Nav.Link href="/login">Login</Nav.Link>
+            </Nav>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar >
   )
 }
 
-export default Navbar
+export default NavbarCustom;
